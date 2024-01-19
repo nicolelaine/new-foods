0f9b90fbc36a8bae4669397dd56bf6d05d68d6ac
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-01-19 15:28:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import NewFoodInput from "./NewFoodInput";
+import "./styles.css";
+
+export default function App() {
+  const [foodsToTry, setFoodsToTry] = useState([])
+    
+  const addFoodItem = (food) => {
+       
+       setFoodsToTry([...foodsToTry, food]);
+    }
+
+  return (
+    <div className="App">
+      <h1>New Foods to Try</h1>
+      <NewFoodInput addFoodItem={addFoodItem} />
+      <ul className="food-list">
+          {foodsToTry.map((food, index) => (
+            <li key={index}>{food}</li>
+          ))}
+      </ul>
+    </div>
+  );
+}
